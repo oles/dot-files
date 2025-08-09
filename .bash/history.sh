37b84b8cb9c79a70f352e48f0b7ca9824468bf5e
Ole Strøm <olestr@pm.me>
2025-08-09 13:07:21 +0200

    chore: add fg to bash history ignore

--- .bash/history.sh
+++ .bash/history.sh
@@ -3,6 +3,6 @@ export HISTFILESIZE=
 export HISTSIZE=
 export HISTTIMEFORMAT="[%F %T] "
 export HISTCONTROL=ignoreboth
-export HISTIGNORE="ls:cdd:exit:ll:ll ..:cd:cd ..:cd ../"
+export HISTIGNORE="ls:cdd:exit:ll:ll ..:cd:cd ..:cd ../:fg"
 export HISTFILE=~/.cache/bash_history
 PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
